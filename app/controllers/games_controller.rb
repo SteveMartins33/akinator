6e39225f3e3afd52b34393545d84e07343cc12d7
Steve MARTINS <stevemartins@MacBook-Pro-de-Steve.local>
2019-04-03 14:45:24 +0200

    svg 3

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,23 @@
 class GamesController < ApplicationController
+  QUESTIONS = [
+"A t-il les yeux clairs ?",
+"Est-ce un homme ?",
+"Est-il vegan ?",
+"Est-il originaire d'Aquitaine ?"
+  ]
+
   def create
   # créer le tableau de réponses vide
-
-  	redirect_to edit_game_path(1), method: :get
-
+    @game = Game.create
+  	redirect_to edit_game_path(@game["id"], question_id: 0), method: :get
   end
 
   def edit
+    @id = params["@id"]
+    @question = QUESTIONS[params["question_id"].to_i]
   end
 
   def update
-  	redirect_to game_path(1), method: :get
   end
 
   def show
